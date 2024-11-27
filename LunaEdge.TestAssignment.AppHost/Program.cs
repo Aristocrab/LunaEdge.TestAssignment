fb8ac5c42f2d58443079c88b89153430f2bc709b
Aristocrab <maksymkanupa@gmail.com>
2024-11-28 01:05:16 +0200

    Update configuration

--- LunaEdge.TestAssignment.AppHost/Program.cs
+++ LunaEdge.TestAssignment.AppHost/Program.cs
@@ -7,6 +7,17 @@ var logger = new LoggerConfiguration()
     .CreateLogger();
 builder.Services.AddSerilog(logger);
 
-builder.AddProject<Projects.LunaEdge_TestAssignment_WebApi>("apiservice");
+var postgres = builder
+    .AddPostgres("postgres")
+    .WithDataVolume(isReadOnly: false)
+    .WithPgWeb();
+
+var db = postgres
+    .AddDatabase("postgresDb");
+
+builder
+    .AddProject<Projects.LunaEdge_TestAssignment_WebApi>("apiservice")
+    .WithReference(db)
+    .WaitFor(postgres);
 
 builder.Build().Run();
--- LunaEdge.TestAssignment.WebApi/Program.cs
+++ LunaEdge.TestAssignment.WebApi/Program.cs
@@ -1,11 +1,33 @@
+using LunaEdge.TestAssignment.Application.Database;
 using LunaEdge.TestAssignment.ServiceDefaults;
 using LunaEdge.TestAssignment.WebApi;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.AddWebServices();
+builder.AddApiServices();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+app.UseExceptionHandler();
+
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate();
+}
+
 app.MapControllers();
 app.MapDefaultEndpoints();
+
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "LunaEdge.TestAssignment.WebApi v1");
+    c.RoutePrefix = string.Empty;
+});
+
+// redirect root to swagger
+app.MapGet("/", () => Results.Redirect("/swagger"));
 
 app.Run();
